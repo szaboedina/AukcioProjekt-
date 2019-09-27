73f864d3efad8b13bbf871e91f07f7150c5ff400
Edina Szabó <szabo.edina.1229@gmail.com>
2019-09-27 10:59:48 +0200

    elsofeladat

---
+++ Festmeny.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AukcioProjekt
+{
+    class Festmeny
+    {
+        string cim;
+        string festo;
+        string stilus;
+        int licitekSzama;
+        int legmagasabbLicit;
+        DateTime legutolsoLicitIdeje;
+        bool elkelt;
+        DateTime now = DateTime.Now;
+
+        public string Cim { get => cim; set => cim = value; }
+        public string Festo { get => festo; set => festo = value; }
+        public string Stilus { get => stilus; set => stilus = value; }
+        public int LicitekSzama { get => licitekSzama; set => licitekSzama = value; }
+        public int LegmagasabbLicit { get => legmagasabbLicit; set => legmagasabbLicit = value; }
+        public DateTime LegutolsoLicitIdeje { get => legutolsoLicitIdeje; set => legutolsoLicitIdeje = value; }
+        public bool Elkelt { get => elkelt; set => elkelt = value; }
+
+        
+
+
+
+        public override string ToString()
+        {
+            if (elkelt)
+            {
+                return festo + ": " + cim + "(" + stilus + ")\nelkelt\n" + legmagasabbLicit + "$ - " + legutolsoLicitIdeje + "(összesen: " + licitekSzama + " db)";
+
+            }
+            else
+            {
+                return festo + ": " + cim + "(" + stilus + ")\n" + legmagasabbLicit + "$ - " + legutolsoLicitIdeje + "(összesen: " + licitekSzama + " db)";
+
+            }
+        }
+
+
+        public void Licit()
+        {
+            if (elkelt)
+            {
+                Console.WriteLine("Már elkelt.");
+            }
+            else if (licitekSzama==0)
+            {
+                legmagasabbLicit += 100;
+                licitekSzama++;
+            }
+            else
+            {
+                legmagasabbLicit =legmagasabbLicit+ (legmagasabbLicit / 10);
+                licitekSzama++;
+                legutolsoLicitIdeje = now;
+
+            }
+        }
+
+
+    }
+}
